581492c5a48fb1e6e35a7fc1dc893c873dd5863f
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2025-04-06 21:50:51 +0300

    finss

--- components/mainPage/CompanyShow.jsx
+++ components/mainPage/CompanyShow.jsx
@@ -1,38 +1,101 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const CompanyShow = () => {
   return (
-    <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
-        <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">You’ll be in good company</h2>
-        <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
-            <Link href="#" class="flex justify-center items-center ">                
-                <Image src="/main-page/bzn.jpg" width={100} height={100} className='dark:hover:text-white' alt='some thing' />
-            </Link>
-            <Link href="#" class="flex justify-center items-center border-2 border-gray-300 rounded-lg p-2 ">
-                      <h3 className='font-bold'>BEYENE LEDA LOUNGE & HOTEL</h3>                                     
-            </Link>
-            <Link href="#" class="flex justify-center items-center border-2 border-gray-300 rounded-lg p-2">
-                      <h3 className='font-bold'>GIRMA GIFAWOSSEN TRADING</h3>                                     
-            </Link>
-            <Link href="#" class="flex justify-center items-center border-2 border-gray-300 rounded-lg p-2">
-                      <h3 className='font-bold'>SAFARICOM ETHIOPIA</h3>                                     
-            </Link>
-            <Link href="#" class="flex justify-center items-center border-2 border-gray-300 rounded-lg p-2">
-                      <h3 className='font-bold'>BEKA TRADING PLC</h3>                                     
-            </Link>
-            <Link href="#" class="flex justify-center items-center border-2 border-gray-300 rounded-lg p-2">
-                      <h3 className='font-bold'>SOKE RESTAURANT</h3>                                     
-            </Link>
-            
-
-      
+    <div className="flex flex-col gap-8">
+         <section class="bg-white dark:bg-gray-900 mx-8 flex flex-col md:flex-row gap-8 justify-center items-center">
+      <Link
+        href="#"
+        class="flex flex-col items-center bg-[#E6E6E7] border border-gray-300 rounded-full shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 px-4"
+      >
+        <Image
+          src="/main-page/inv-image.svg"
+          class=" rounded-t-lg rounded-l-3xl  md:rounded-l-full md:rounded-s-lg bg-#1066A8"
+          alt="inv-image"
+          width={200}
+          height={200}
+        />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Advanced Inventory Managment
+          </h5>
+          <p class="mb-3 font-normal text-[#172554] dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
         </div>
+      </Link>
+      <Link
+        href="#"
+        class="flex flex-col items-center bg-[#E6E6E7] border border-gray-300 rounded-full shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 px-4"
+      >
+        <Image
+          src="/main-page/flit-image.svg"
+          class=" rounded-t-lg rounded-l-3xl  md:rounded-l-full md:rounded-s-lg bg-#1066A8"
+          alt="inv-image"
+          width={200}
+          height={200}
+        />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Flight Managment System & Spare Parts Managment
+          </h5>
+          <p class="mb-3 font-normal text-[#172554] dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </div>
+      </Link>
+    </section>
+         <section class="bg-white dark:bg-gray-900 mx-8 flex flex-col md:flex-row gap-8 justify-center items-center">
+      <Link
+        href="#"
+        class="flex flex-col items-center bg-[#E6E6E7] border border-gray-300 rounded-full shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 px-4"
+      >
+        <Image
+          src="/main-page/flit-image.svg"
+          class=" rounded-t-lg rounded-l-3xl  md:rounded-l-full md:rounded-s-lg bg-#1066A8"
+          alt="inv-image"
+          width={200}
+          height={200}
+        />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+           Engaging Website For Your Business
+          </h5>
+          <p class="mb-3 font-normal text-[#172554] dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </div>
+      </Link>
+      <Link
+        href="#"
+        class="flex flex-col items-center bg-[#E6E6E7] border border-gray-300 rounded-full shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 px-4"
+      >
+        <Image
+          src="/main-page/inv-image.svg"
+          class=" rounded-t-lg rounded-l-3xl  md:rounded-l-full md:rounded-s-lg bg-#1066A8"
+          alt="inv-image"
+          width={200}
+          height={200}
+        />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p class="mb-3 font-normal text-[#172554] dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </div>
+      </Link>
+    </section>
     </div>
-</section>
-  )
-}
+ 
+  );
+};
 
-export default CompanyShow
+export default CompanyShow;
